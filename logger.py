8aa3d135e05977bf29f92289159e930a07bffda0
lalhrinthang <lalhrinthang@gmail.com>
2026-03-14 02:46:33 +0800

    Add logging configuration to logger module

--- logger.py
+++ logger.py
@@ -1,3 +1,12 @@
 import logging
 from datetime import datetime # for timestamping log entries
 
+# Configure logging
+logging.basicConfig(
+    level=logging.INFO, # Set the logging level to INFO
+    format='%(asctime)s | %(levelname)s | %(message)s', # Log format includes timestamp, log level, and message
+    handlers=[
+        logging.FileHandler('logfile.txt'), # Log to a file named 'syndicate_pipeline.log'
+        logging.StreamHandler() # Also log to the console
+    ]
+)
