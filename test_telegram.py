7016570b8e7b9112816676f7b555f673dff75367
lalhrinthang <lalhrinthang@gmail.com>
2026-03-14 03:00:36 +0800

    Testing Telegram Bot

--- test_telegram.py
+++ test_telegram.py
@@ -1,3 +1,3 @@
 from modules.telegram_bot import send_alert
 
-send_alert("✅ Syndicate Pipeline Bot is working! Hello from Python!")
+send_alert("✅ Syndicate Pipeline Bot is working! HI from Python!")
